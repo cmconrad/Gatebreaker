9645cb5e908cf7f1d126ea0e7198aaf4cdf81acc
rhstuthers <rhstuthers@anderson.edu>
2019-12-09 12:36:42 -0500

    Level Controller
    Level Controller added for scene transitions when player meets the doors. Still waiting to add the warrior to the other scenes in case you change him, but the transistions do work fine. Warrior will have to be tagged as "Player" once placed in all of the scenes.

---
+++ Gatebreaker/Assets/Scripts/LevelControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class LevelControl : MonoBehaviour
+{
+    public int index;
+    public string levelName;
+
+    void OnTriggerEnter2D(Collider2D door)
+    {
+        if (door.CompareTag("Player"))
+        {
+            //Load level with build index
+            SceneManager.LoadScene(index);
+
+            //Load level with scene name
+            //SceneManager.LoadScene(levelName);
+
+
+        }
+    }
+}
