77d14413c1db1eeceef2dab0c2e98fce78ceef37
Caleb Conrad <cmconrad@anderson.edu>
2019-12-09 13:45:35 -0500

    Added charachter to each level

---
+++ Gatebreaker/Assets/Scripts/Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public Canvas canvas;
+
+    public void ClickMe()
+    {
+        Destroy(canvas);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
